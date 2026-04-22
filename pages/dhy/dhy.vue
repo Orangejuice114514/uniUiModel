fef1219c7f3c28e14ea5bb9cbb6ba2f8f24a0337
orangeawa <orangeawa@outlook.com>
2026-04-22 17:40:00 +0800

    111

---
+++ pages/dhy/dhy.vue
@@ -0,0 +1,28 @@
+<template>
+	<view>
+		<swiper>
+			<swiper-item>
+				<div></div>
+			</swiper-item>
+			<swiper-item>2</swiper-item>
+			<swiper-item>3</swiper-item>
+		</swiper>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
